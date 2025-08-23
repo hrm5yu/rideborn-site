623dae6fdd35a1726c6140552720dfe809832461
hrm5yu <syugoru@gmail.com>
2025-08-23 18:06:40 +0900

    Fix GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rideborn.jp',
+  site: 'https://あなたのユーザー名.github.io',
+  base: '/rideborn-site',
   output: 'static', // 静的サイトとして出力
   build: {
     assets: 'assets'
